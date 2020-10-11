740ba80b2a6436526c069cd3f422d3795537e25b
Petr Horacek <phoracek@redhat.com>
2020-10-11 15:13:40 +0200

    ui: Add system helpers and basic structure
    Not much, but now we have some basic elements (for fun) and most
    importantly, system helpers to get things started. We have a UI!
    
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

---
+++ gazpatcho-ui/src/main.rs
@@ -0,0 +1,63 @@
+mod system;
+
+use std::ptr;
+
+use imgui::*;
+
+fn main() {
+    let s = system::System::init("Gazpatcho");
+    s.main_loop(move |_, ui| {
+        let style_vars = ui.push_style_vars(&[
+            StyleVar::WindowRounding(0.0),
+            StyleVar::ChildRounding(0.0),
+            StyleVar::FrameRounding(0.0),
+            StyleVar::GrabRounding(0.0),
+            StyleVar::PopupRounding(0.0),
+            StyleVar::ScrollbarRounding(0.0),
+        ]);
+
+        let style_color = ui.push_style_color(StyleColor::WindowBg, [0.2, 0.2, 0.2, 1.0]);
+
+        Window::new(im_str!("Hello world"))
+            .position([0.0, 0.0], Condition::Always)
+            .size(ui.io().display_size, Condition::Always)
+            .title_bar(false)
+            .resizable(false)
+            .always_auto_resize(true)
+            .movable(false)
+            .build(ui, || {
+                if unsafe { imgui_sys::igBeginPopupContextWindow(ptr::null(), 1) } {
+                    MenuItem::new(im_str!("Load")).build(ui);
+                    MenuItem::new(im_str!("Save as")).build(ui);
+                    ui.separator();
+                    MenuItem::new(im_str!("Sound output")).build(ui);
+                    MenuItem::new(im_str!("Mixer")).build(ui);
+                    MenuItem::new(im_str!("Oscillator")).build(ui);
+                    unsafe { imgui_sys::igEndPopup() };
+                }
+
+                ChildWindow::new("child")
+                    .size([400.0, 200.0])
+                    .border(true)
+                    .build(ui, || {
+                        for i in 0..10 {
+                            ui.text(format!("Scrolling Text {}", i));
+                        }
+                    });
+
+                ui.same_line(0.0);
+
+                ChildWindow::new("child2")
+                    .size([0.0, 400.0])
+                    .border(true)
+                    .build(ui, || {
+                        for i in 0..10 {
+                            ui.text(format!("Scrolling Text {}", i));
+                        }
+                    });
+            });
+
+        style_vars.pop(ui);
+        style_color.pop(ui);
+    });
+}
