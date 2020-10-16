c2f1c7531977bd2c72d673c5494484937454f57b
Petr Horacek <phoracek@redhat.com>
2020-10-16 15:34:27 +0200

    Add more Node abstraction
    It is even bigger mess, but it will converge.
    
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- src/internal.rs
+++ src/internal.rs
@@ -42,6 +42,9 @@ pub struct Pin {
 // TODO: Split it to registering of input and taken actions
 impl Node {
     pub fn build(&mut self, ui: &imgui::Ui<'_>, position_offset: &Vec2) {
+        // TODO: NodeState
+        // TODO: Draw
+        // TODO: Act
         let draw_list = ui.get_window_draw_list();
 
         let node_size = [
@@ -56,80 +59,12 @@ impl Node {
             },
         ];
 
-        // Invisible button for the whole node, for dragging
         self.build_background_button(ui, &node_size, position_offset);
-
-        // Draw the box background
         self.build_background(&draw_list, &node_size, position_offset);
-
-        // Draw title
         self.build_title(&draw_list, position_offset);
-
-        // Draw input pins
-        for (i, pin) in self.input_pins.iter().enumerate() {
-            build_pin(
-                ui,
-                &draw_list,
-                pin,
-                PinSide::Input,
-                self.position,
-                &self.class,
-                &self.id,
-                i,
-                position_offset,
-            );
-        }
-
-        for (i, pin) in self.output_pins.iter().enumerate() {
-            let highlight_width =
-                ui.calc_text_size(&imgui::ImString::new(&pin.label), false, 0.0)[0] + 18.0;
-
-            let pin_position = self.position + [node_size[0], 35.0 + 27.0 * (i as f32)];
-
-            ui.set_cursor_screen_pos(
-                (pin_position + *position_offset + [-highlight_width, 0.0]).into(),
-            );
-            ui.invisible_button(
-                &imgui::ImString::new(&format!("{}{}{}", self.class, self.id, pin.class)),
-                [highlight_width, 17.0],
-            );
-            if ui.is_item_hovered() {
-                draw_list
-                    .add_rect(
-                        (pin_position + *position_offset).into(),
-                        (pin_position + [-highlight_width, 17.0] + *position_offset).into(),
-                        GRAY,
-                    )
-                    .filled(true)
-                    .build();
-            }
-
-            draw_list
-                .add_rect(
-                    (pin_position + *position_offset).into(),
-                    (pin_position + [-3.0, 17.0] + *position_offset).into(),
-                    BLACK,
-                )
-                .filled(true)
-                .build();
-
-            draw_list.add_text(
-                (pin_position + [-highlight_width + 8.0, 1.0] + *position_offset).into(),
-                BLACK,
-                &imgui::ImString::new(&pin.label),
-            );
-        }
-
-        // Draw edges of the box
-        {
-            draw_list
-                .add_rect(
-                    (self.position + *position_offset).into(),
-                    (self.position + node_size + *position_offset).into(),
-                    BLACK,
-                )
-                .build();
-        }
+        self.build_input_pins(ui, &draw_list, position_offset);
+        self.build_output_pins(ui, &draw_list, position_offset, &node_size);
+        self.build_node_edges(&draw_list, position_offset, &node_size);
     }
 
     fn build_background_button(
@@ -175,58 +110,154 @@ impl Node {
             &imgui::ImString::new(&self.label),
         );
     }
-}
 
-enum PinSide {
-    Input,
-    Output,
-}
+    fn build_input_pins(
+        &self,
+        ui: &imgui::Ui<'_>,
+        draw_list: &imgui::WindowDrawList,
+        position_offset: &Vec2,
+    ) {
+        for (i, pin) in self.input_pins.iter().enumerate() {
+            let pin_position = self.position + [0.0, 35.0 + 27.0 * (i as f32)];
 
-fn build_pin(
-    ui: &imgui::Ui<'_>,
-    draw_list: &imgui::WindowDrawList,
-    pin: &Pin,
-    _side: PinSide,
-    node_position: Vec2,
-    node_class: &str,
-    node_id: &str,
-    index: usize,
-    position_offset: &Vec2,
-) {
-    let pin_position = node_position + [0.0, 35.0 + 27.0 * (index as f32)];
+            self.build_pin_highlight(ui, draw_list, pin, &pin_position, position_offset);
+            self.build_pin_marker_left(ui, draw_list, pin, &pin_position, position_offset);
+            self.build_pin_label_left(ui, draw_list, pin, &pin_position, position_offset);
+        }
+    }
 
-    let highlight_width = pin_box_size(ui, pin).x;
+    fn build_output_pins(
+        &self,
+        ui: &imgui::Ui<'_>,
+        draw_list: &imgui::WindowDrawList,
+        position_offset: &Vec2,
+        node_size: &[f32; 2],
+    ) {
+        for (i, pin) in self.input_pins.iter().enumerate() {
+            let highlight_width = pin_box_size(ui, pin).x;
+            let pin_position =
+                self.position + [node_size[0] - highlight_width, 35.0 + 27.0 * (i as f32)];
 
-    ui.set_cursor_screen_pos((pin_position + *position_offset).into());
-    ui.invisible_button(
-        &imgui::ImString::new(&format!("{}{}{}", node_class, node_id, pin.class)),
-        [highlight_width, 17.0],
-    );
-    if ui.is_item_hovered() {
+            self.build_pin_highlight(ui, draw_list, pin, &pin_position, position_offset);
+            self.build_pin_marker_right(
+                ui,
+                draw_list,
+                pin,
+                &pin_position,
+                position_offset,
+                highlight_width,
+            );
+            self.build_pin_label_right(ui, draw_list, pin, &pin_position, position_offset);
+        }
+    }
+
+    fn build_pin_highlight(
+        &self,
+        ui: &imgui::Ui<'_>,
+        draw_list: &imgui::WindowDrawList,
+        pin: &Pin,
+        pin_position: &Vec2,
+        position_offset: &Vec2,
+    ) {
+        let highlight_width = pin_box_size(ui, pin).x;
+        ui.set_cursor_screen_pos((*pin_position + *position_offset).into());
+        ui.invisible_button(
+            &imgui::ImString::new(&format!("{}{}{}", self.class, self.id, pin.class)),
+            [highlight_width, 17.0],
+        );
+        if ui.is_item_hovered() {
+            draw_list
+                .add_rect(
+                    (*pin_position + *position_offset).into(),
+                    (*pin_position + [highlight_width, 17.0] + *position_offset).into(),
+                    GRAY,
+                )
+                .filled(true)
+                .build();
+        }
+    }
+
+    fn build_pin_marker_left(
+        &self,
+        ui: &imgui::Ui<'_>,
+        draw_list: &imgui::WindowDrawList,
+        pin: &Pin,
+        pin_position: &Vec2,
+        position_offset: &Vec2,
+    ) {
         draw_list
             .add_rect(
-                (pin_position + *position_offset).into(),
-                (pin_position + [highlight_width, 17.0] + *position_offset).into(),
-                GRAY,
+                (*pin_position + *position_offset).into(),
+                (*pin_position + [3.0, 17.0] + *position_offset).into(),
+                BLACK,
             )
             .filled(true)
             .build();
     }
 
-    draw_list
-        .add_rect(
-            (pin_position + *position_offset).into(),
-            (pin_position + [3.0, 17.0] + *position_offset).into(),
-            BLACK,
-        )
-        .filled(true)
-        .build();
+    fn build_pin_marker_right(
+        &self,
+        ui: &imgui::Ui<'_>,
+        draw_list: &imgui::WindowDrawList,
+        pin: &Pin,
+        pin_position: &Vec2,
+        position_offset: &Vec2,
+        highlight_width: f32,
+    ) {
+        draw_list
+            .add_rect(
+                (*pin_position + *position_offset + [highlight_width, 0.0]).into(),
+                (*pin_position + [highlight_width - 3.0, 17.0] + *position_offset).into(),
+                BLACK,
+            )
+            .filled(true)
+            .build();
+    }
 
-    draw_list.add_text(
-        (pin_position + [PIN_PADDING_OUTER, 1.0] + *position_offset).into(),
-        BLACK,
-        &imgui::ImString::new(&pin.label),
-    );
+    fn build_pin_label_left(
+        &self,
+        ui: &imgui::Ui<'_>,
+        draw_list: &imgui::WindowDrawList,
+        pin: &Pin,
+        pin_position: &Vec2,
+        position_offset: &Vec2,
+    ) {
+        draw_list.add_text(
+            (*pin_position + [PIN_PADDING_OUTER, 1.0] + *position_offset).into(),
+            BLACK,
+            &imgui::ImString::new(&pin.label),
+        );
+    }
+
+    fn build_pin_label_right(
+        &self,
+        ui: &imgui::Ui<'_>,
+        draw_list: &imgui::WindowDrawList,
+        pin: &Pin,
+        pin_position: &Vec2,
+        position_offset: &Vec2,
+    ) {
+        draw_list.add_text(
+            (*pin_position + [PIN_PADDING_INNER, 1.0] + *position_offset).into(),
+            BLACK,
+            &imgui::ImString::new(&pin.label),
+        );
+    }
+
+    fn build_node_edges(
+        &self,
+        draw_list: &imgui::WindowDrawList,
+        position_offset: &Vec2,
+        node_size: &[f32; 2],
+    ) {
+        draw_list
+            .add_rect(
+                (self.position + *position_offset).into(),
+                (self.position + *node_size + *position_offset).into(),
+                BLACK,
+            )
+            .build();
+    }
 }
 
 fn pin_box_size(ui: &imgui::Ui<'_>, pin: &Pin) -> Vec2 {
