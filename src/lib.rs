3120429405fcf983afbc48cbfc3d7e73cf1c628b
Petr Horáček <phoracek@redhat.com>
2020-10-24 21:52:26 +0200

    Be explicit about imgui package in lib
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#[macro_use]
 extern crate imgui;
 
 pub mod config;
@@ -7,10 +8,8 @@ mod system;
 mod vec2;
 mod widget;
 
-use imgui::*;
-
-pub fn run(config_: config::Config) {
-    let mut model = model::Model::new(config_);
+pub fn run(configuration: config::Config) {
+    let mut model = model::Model::new(configuration);
 
     let s = system::System::init("Gazpatcho");
     s.main_loop(move |_, ui| {
@@ -20,23 +19,23 @@ pub fn run(config_: config::Config) {
     });
 }
 
-fn set_styles<F: FnOnce()>(ui: &Ui<'_>, f: F) {
+fn set_styles<F: FnOnce()>(ui: &imgui::Ui<'_>, f: F) {
     let style_vars = ui.push_style_vars(&[
-        StyleVar::WindowRounding(0.0),
-        StyleVar::ChildRounding(0.0),
-        StyleVar::FrameRounding(0.0),
-        StyleVar::GrabRounding(0.0),
-        StyleVar::PopupRounding(0.0),
-        StyleVar::ScrollbarRounding(0.0),
+        imgui::StyleVar::WindowRounding(0.0),
+        imgui::StyleVar::ChildRounding(0.0),
+        imgui::StyleVar::FrameRounding(0.0),
+        imgui::StyleVar::GrabRounding(0.0),
+        imgui::StyleVar::PopupRounding(0.0),
+        imgui::StyleVar::ScrollbarRounding(0.0),
     ]);
 
     let style_colors = ui.push_style_colors(&[
-        (StyleColor::WindowBg, [1.0, 1.0, 1.0, 1.0]),
-        (StyleColor::Text, [0.0, 0.0, 0.0, 1.0]),
-        (StyleColor::PopupBg, [1.0, 1.0, 1.0, 1.0]),
-        (StyleColor::HeaderHovered, [0.9, 0.9, 0.9, 1.0]),
-        (StyleColor::Separator, [0.0, 0.0, 0.0, 1.0]),
-        (StyleColor::Border, [0.0, 0.0, 0.0, 1.0]),
+        (imgui::StyleColor::WindowBg, [1.0, 1.0, 1.0, 1.0]),
+        (imgui::StyleColor::Text, [0.0, 0.0, 0.0, 1.0]),
+        (imgui::StyleColor::PopupBg, [1.0, 1.0, 1.0, 1.0]),
+        (imgui::StyleColor::HeaderHovered, [0.9, 0.9, 0.9, 1.0]),
+        (imgui::StyleColor::Separator, [0.0, 0.0, 0.0, 1.0]),
+        (imgui::StyleColor::Border, [0.0, 0.0, 0.0, 1.0]),
     ]);
 
     f();
@@ -45,10 +44,10 @@ fn set_styles<F: FnOnce()>(ui: &Ui<'_>, f: F) {
     style_colors.pop(ui);
 }
 
-fn show_main_window(ui: &Ui<'_>, model: &mut model::Model) {
-    Window::new(im_str!("Gazpatcho"))
-        .position([0.0, 0.0], Condition::Always)
-        .size(ui.io().display_size, Condition::Always)
+fn show_main_window(ui: &imgui::Ui<'_>, model: &mut model::Model) {
+    imgui::Window::new(im_str!("Gazpatcho"))
+        .position([0.0, 0.0], imgui::Condition::Always)
+        .size(ui.io().display_size, imgui::Condition::Always)
         .always_auto_resize(true)
         .movable(false)
         .resizable(false)
