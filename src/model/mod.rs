888eca036cbcec490e139685bba343f6acb7554d
Petr Horáček <phoracek@redhat.com>
2020-10-24 17:54:14 +0200

    Move patch drawing to its module
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,3 +1,4 @@
+// TODO: Keep canvas as a part of this
 extern crate imgui;
 
 pub mod node;
@@ -29,28 +30,6 @@ impl Model {
 
     pub fn draw(&mut self, ui: &imgui::Ui, canvas_offset: [f32; 2]) {
         let active_pin = self.draw_nodes(ui, canvas_offset);
-
-        if let Some(last_active_pin) = &self.last_active_pin {
-            self.draw_patch_draft(ui, &last_active_pin);
-        }
-
-        self.draw_patches(ui);
-
-        if ui.is_mouse_clicked(imgui::MouseButton::Left) {
-            let mut new_patch = None;
-
-            self.last_active_pin = match (&self.last_active_pin, &active_pin) {
-                (Some(last_active_pin), Some(active_pin)) => {
-                    new_patch = Some(Patch::new(*last_active_pin, *active_pin));
-                    None
-                }
-                (None, Some(active_pin)) => Some(*active_pin),
-                (_, None) => None,
-            };
-
-            if let Some(new_patch) = new_patch {
-                self.add_patch(new_patch);
-            }
-        }
+        self.draw_patches(ui, &active_pin);
     }
 }
--- src/model/patch.rs
+++ src/model/patch.rs
@@ -14,24 +14,49 @@ impl Model {
         &self.patches
     }
 
-    pub fn draw_patches(&self, ui: &imgui::Ui) {
+    pub fn draw_patches(&mut self, ui: &imgui::Ui, active_pin: &Option<PinAddress>) {
         for patch in self.patches().iter() {
-            let source = self.get_pin(patch.source()).unwrap().patch_position();
-            let destination = self.get_pin(patch.destination()).unwrap().patch_position();
+            self.draw_patch(ui, patch);
+        }
+
+        self.draw_patch_draft(ui);
+
+        if ui.is_mouse_clicked(imgui::MouseButton::Left) {
+            let mut new_patch = None;
+
+            self.last_active_pin = match (&self.last_active_pin, &active_pin) {
+                (Some(last_active_pin), Some(active_pin)) => {
+                    new_patch = Some(Patch::new(*last_active_pin, *active_pin));
+                    None
+                }
+                (None, Some(active_pin)) => Some(*active_pin),
+                (_, None) => None,
+            };
+
+            if let Some(new_patch) = new_patch {
+                self.add_patch(new_patch);
+            }
+        }
+    }
+
+    fn draw_patch(&self, ui: &imgui::Ui, patch: &Patch) {
+        let source = self.get_pin(patch.source()).unwrap().patch_position();
+        let destination = self.get_pin(patch.destination()).unwrap().patch_position();
+        let draw_list = ui.get_window_draw_list();
+        draw_list
+            .add_line(source, destination, [0.0, 0.0, 0.0])
+            .build();
+    }
+
+    fn draw_patch_draft(&self, ui: &imgui::Ui) {
+        if let Some(last_active_pin) = &self.last_active_pin {
+            let source = self.get_pin(last_active_pin).unwrap().patch_position();
+            let destination = ui.io().mouse_pos;
             let draw_list = ui.get_window_draw_list();
             draw_list
                 .add_line(source, destination, [0.0, 0.0, 0.0])
                 .build();
         }
-    }
-
-    pub fn draw_patch_draft(&self, ui: &imgui::Ui, last_active_pin: &PinAddress) {
-        let source = self.get_pin(last_active_pin).unwrap().patch_position();
-        let destination = ui.io().mouse_pos;
-        let draw_list = ui.get_window_draw_list();
-        draw_list
-            .add_line(source, destination, [0.0, 0.0, 0.0])
-            .build();
     }
 }
 
