5e86166e374d438b7d148dd225dc659be1aa63cc
Petr Horáček <phoracek@redhat.com>
2020-11-09 18:30:14 +0100

    Make Store specific
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- src/store.rs
+++ src/store.rs
@@ -1,61 +1,21 @@
-// TODO: Make this specific, no need for a framework
-pub struct Store<S, A> {
-    state: S,
-    reducer: fn(&mut S, A),
+use crate::action::Action;
+use crate::state::State;
+
+pub struct Store {
+    state: State,
+    reducer: fn(&mut State, Action),
 }
 
-impl<S, A> Store<S, A> {
-    pub fn new(state: S, reducer: fn(&mut S, A)) -> Self {
+impl Store {
+    pub fn new(state: State, reducer: fn(&mut State, Action)) -> Self {
         Self { state, reducer }
     }
 
-    pub fn state(&self) -> &S {
+    pub fn state(&self) -> &State {
         &self.state
     }
 
-    pub fn reduce(&mut self, action: A) {
+    pub fn reduce(&mut self, action: Action) {
         (self.reducer)(&mut self.state, action);
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    struct State {
-        number: i32,
-    }
-
-    enum Action {
-        Add(i32),
-    }
-
-    fn reduce(state: &mut State, action: Action) {
-        match action {
-            Action::Add(num) => {
-                state.number += num;
-            }
-        };
-    }
-
-    #[test]
-    fn initialize_store() {
-        let _store = Store::new(State { number: 0 }, reduce);
-    }
-
-    #[test]
-    fn get_state() {
-        let store = Store::new(State { number: 0 }, reduce);
-
-        assert_eq!(store.state().number, 0);
-    }
-
-    #[test]
-    fn reduce_on_action() {
-        let mut store = Store::new(State { number: 0 }, reduce);
-
-        store.reduce(Action::Add(1));
-
-        assert_eq!(store.state().number, 1);
     }
 }
