18d905af61c30ffd936888eed6f30e18fb25e00d
Petr Horáček <phoracek@redhat.com>
2020-11-18 14:20:14 +0100

    Documentation for widgets
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- src/widget/mod.rs
+++ src/widget/mod.rs
@@ -1,3 +1,7 @@
+//! Custom imgui Node widget implementation. This represents a node in a graph.
+//! Apart from input/output pins it also provides several widgets allowing users
+//! to set arbitrary values per each node.
+
 pub mod dropdown;
 pub mod label;
 pub mod multiline_input;
