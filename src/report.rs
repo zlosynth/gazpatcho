7f0e14ee45ff24b22f865af99d132c5286d5ccd6
Petr Horáček <phoracek@redhat.com>
2020-11-18 16:05:27 +0100

    Add report documentation
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- src/report.rs
+++ src/report.rs
@@ -1,19 +1,32 @@
-// TODO: User docs
+//! Definition of the current state of the graph modeled in the UI.
+
 use std::collections::HashMap;
 
+/// Report is a structure holding information about the current "model" of the
+/// graph represented in the UI. It does not report details about the widgets
+/// that were used nor about positions of items on the canvas. It is limited to
+/// the minimal amount of information needed to convert the state into a graph.
 #[derive(Debug)]
 pub struct Report {
+    /// All instantiated notes with their values set via widgets.
     pub nodes: Vec<Node>,
+    /// List of all patches connecting node pins.
     pub patches: Vec<Patch>,
 }
 
+/// A structure representing an existing node and its associated values.
 #[derive(Debug)]
 pub struct Node {
+    /// Unique identificator of this node.
     pub id: String,
+    /// Class references the node template this node was instantiated from.
     pub class: String,
+    /// Map of all the values set via widgets available on the node. The key is
+    /// always the key of the given widget as defined in the node template.
     pub data: HashMap<String, Value>,
 }
 
+/// Enum encapsulating possible values of an item attached to a node.
 #[derive(Debug)]
 pub enum Value {
     String(String),
@@ -22,6 +35,11 @@ pub enum Value {
 }
 
 impl Value {
+    /// Access string value stored in the enum.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the variant is not `String`.
     pub fn unwrap_string(&self) -> &str {
         if let Self::String(value) = self {
             &value
@@ -30,6 +48,11 @@ impl Value {
         }
     }
 
+    /// Access f32 value stored in the enum.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the variant is not `F32`.
     pub fn unwrap_f32(&self) -> f32 {
         if let Self::F32(value) = self {
             *value
@@ -38,6 +61,11 @@ impl Value {
         }
     }
 
+    /// Access bool value stored in the enum.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the variant is not `Bool`.
     pub fn unwrap_bool(&self) -> bool {
         if let Self::Bool(value) = self {
             *value
@@ -47,12 +75,17 @@ impl Value {
     }
 }
 
+/// A structure representing a patch between two pins of available nodes.
 #[derive(Debug)]
 pub struct Patch {
+    /// Output pin where the patch originates from.
     pub source: PinAddress,
+    /// Input pin where the patch leads into.
     pub destination: PinAddress,
 }
 
+/// Uniquely represents a pin by referencing node by its ID an pin by
+/// its class.
 #[derive(Debug)]
 pub struct PinAddress {
     pub node_id: String,
